d3a2c9a9d7fc8545c76aa1542aac8a736100192a
Ionut Ciovica <ionut.ciovica@gmail.com>
2023-12-15 10:14:47 +0200

    Initial Commit

---
+++ Pago-SwiftUI/Pago_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pago_SwiftUIApp.swift
+//  Pago-SwiftUI
+//
+//  Created by IonutCiovica on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pago_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
